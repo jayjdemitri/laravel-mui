8616fc45dfe085e2173cdad193b307d5572792d7
jayjdemitri <jaydemitri24@gmail.com>
2024-07-28 11:08:38 +0100

    Updated and removed unsued imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
@@ -10,13 +9,7 @@ export default {
         './resources/js/**/*.tsx',
     ],
 
-    theme: {
-        // extend: {
-        //     fontFamily: {
-        //         sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-        //     },
-        // },
-    },
+    theme: { },
 
     plugins: [forms],
 };
