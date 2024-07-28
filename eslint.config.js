cf96455e051bca1ae2ba6ffbfb2c5cd48a821ea0
jayjdemitri <jaydemitri24@gmail.com>
2024-07-28 11:08:14 +0100

    Added eslint config

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  {
+    ignores: ['vendor/**', 'public/**']
+  },
+  {
+    //files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
+    files: ["**/*.{ts,jsx,tsx}"]
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "script" }
+  },
+  {
+    languageOptions: { globals: globals.browser }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
+];
