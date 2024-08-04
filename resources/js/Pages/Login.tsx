52e5e4e40fe074a535701d2ac206c153dd0f44a1
jayjdemitri <jaydemitri24@gmail.com>
2024-08-04 19:01:04 +0100

    Updated basic login format to inclue the nav

--- resources/js/Pages/Login.tsx
+++ resources/js/Pages/Login.tsx
@@ -1,115 +1,23 @@
+import NavBar, { Offset } from '@/Components/Navigation/NavBar';
+import EmptyLayout from '@/Layouts/EmptyLayout';
 import { Head } from '@inertiajs/react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import CenteredLayout from '@/Layouts/CenteredLayout';
-import { Paper } from '@mui/material';
-
-function Copyright(props: any) {
-    return (
-        <Typography variant="body2" color="text.secondary" align="center" {...props}>
-            {'Copyright © '}
-            <Link color="inherit" href="https://mui.com/">
-                Your Website
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
-}
+import { Box, Container, Typography } from '@mui/material';
 
 const Login = () => {
-
     const title = 'Login';
-
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
-    };
-
-
     return (
-        <CenteredLayout>
-            <Container component="main" maxWidth="xs">
-                <Head title={title} />
-
-
-
-                <Box>
-                    <Paper sx={{ padding: 3, display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
-                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                            <LockOutlinedIcon />
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
-                            Sign in
-                        </Typography>
-                        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                                variant="outlined"
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                                variant="outlined"
-                            />
-                            <FormControlLabel
-                                control={<Checkbox value="remember" color="primary" />}
-                                label="Remember me"
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Sign In
-                            </Button>
-                            <Grid container>
-                                <Grid item xs>
-                                    <Link href="#" variant="body2">
-                                        Forgot password?
-                                    </Link>
-                                </Grid>
-                                <Grid item>
-                                    <Link href="#" variant="body2">
-                                        {"Don't have an account? Sign Up"}
-                                    </Link>
-                                </Grid>
-                            </Grid>
-                        </Box>
-                    </Paper>
+        <EmptyLayout>
+            <Head title={title} />
+            <NavBar hasBack />
+            <Offset />
+            <Container maxWidth='lg'>
+                <Box textAlign='center' padding={12}>
+                    <Typography variant="h1">
+                        {title} Page
+                    </Typography>
                 </Box>
-
-                <Copyright sx={{ mt: 8, mb: 4 }} />
             </Container>
-        </CenteredLayout>
+        </EmptyLayout>
     )
 }
 
