419a3623898c63a3c59894e77bcae8d45a0eafc4
jayjdemitri <jaydemitri24@gmail.com>
2024-08-04 19:04:09 +0100

    Updated basic register format to inclue the nav

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,16 +1,19 @@
 import EmptyLayout from '@/Layouts/EmptyLayout';
-import { Head, Link } from '@inertiajs/react';
-import { Button } from '@mui/material';
+import { Head } from '@inertiajs/react';
+import { Box, Container, Typography } from '@mui/material';
+
 const Home = () => {
     const title = 'Home';
     return (
         <EmptyLayout>
             <Head title={title} />
-            {title} Page
-            <br />
-            <Link href="/">
-                <Button variant="outlined">Home</Button>
-            </Link>
+            <Container maxWidth='lg'>
+                <Box textAlign='center' padding={12}>
+                    <Typography variant="h1">
+                        {title} Page
+                    </Typography>
+                </Box>
+            </Container>
         </EmptyLayout>
     )
 }
